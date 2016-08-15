15279139f0cb360f287d874c1bdb4b57b5c4b874
olgatsiamliak <o.tsiamliak@gmail.com>
2016-08-15 17:02:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function Pagination (rootSelector, total, current) {
-	this.root = $(rootSelector); // селектор контейнера
-	this.total = total; // итого страниц	
-	var current =  current || 0; // если значение не задано, то = 0
+	this.root = $(rootSelector); 
+	this.total = total; 
+	var current =  current || 0;
 	this.current = function (value) {
 		if (!arguments.length) return current;
 		if (typeof(value) != 'number') return;
@@ -14,38 +14,38 @@ function Pagination (rootSelector, total, current) {
 		}
 		  
 	}
-	this.render(); // метод, который генерирует разметку компонента 
-	this.installListeners(); // метод, который расставляет все орбаботчики, делаем однократно
+	this.render(); 
+	this.installListeners(); 
 
 }
 
 Pagination.prototype = {
 	render: function () {
 		var current = this.current();
-		var root = this.root; // записали в переменную текущий контекст this, чтобы в ф-ции renderLink он был доступен
+		var root = this.root; 
 		function renderLink (params) { 
-			var li = $('<li><a></a></li>'); // шаблон, который верен для всех элементов нашего pagination
+			var li = $('<li><a></a></li>'); 
 			switch (params.type) {
-				case 'dots': li.addClass('disabled').children('a').text('...'); // если тип многоточие, то добавляем класс disabled (чтоб ссыль была не кликабельна)
+				case 'dots': li.addClass('disabled').children('a').text('...'); 
 							break;
-				case 'number': if (params.active) li.addClass('active'); // если есть в объекте параметр active, то вешаем класс
-							   else li.children('a').attr('href', '#'); // если нет активный, то добавляем атрибут href
-							   li.children('a').text(params.value); // записываем контент
+				case 'number': if (params.active) li.addClass('active'); 
+							   else li.children('a').attr('href', '#'); 
+							   li.children('a').text(params.value); 
 						   break;
-				case 'arrow': if (params.value < 0) li.addClass('previous').children('a').html('&laquo;'); // если стрелки, то добавляем класс следующий/ предыдущий и текст
+				case 'arrow': if (params.value < 0) li.addClass('previous').children('a').html('&laquo;'); 
 								else li.addClass('next').children('a').html('&raquo;'); 
-								if (params.disabled) li.addClass('disabled'); // если есть параметр disable, т.е. активные крайние элементы, то делаем не кликабельными стрелки
-								else li.children('a').attr('href', '#'); // в остальных случая добавляем атрибут href
+								if (params.disabled) li.addClass('disabled'); 
+								else li.children('a').attr('href', '#'); 
 			}
 			root.append(li);
 
 		}
 		this.root.empty();
-		var centerLeft = Math.max(1, current -2); // левая сторона от активного элемента (1 - если крайнее значение активно)
-		var centerRight = Math.min(this.total - 2, current + 2); // правая стороны от активного элемента (итого - 2 для крайнего элемента)
-        var leftDots = centerLeft > 2; // многоточие появляется если разница больше двух элементов
+		var centerLeft = Math.max(1, current -2); 
+		var centerRight = Math.min(this.total - 2, current + 2); 
+        var leftDots = centerLeft > 2; 
         var rightDots = centerRight < this.total - 3; 
-        if (centerLeft == 2) centerLeft--; // если разница два элемента, значит ставим элемент, вместо многоточия
+        if (centerLeft == 2) centerLeft--; 
         if (centerRight == this.total - 3) centerRight++;
 
 		renderLink({
@@ -87,20 +87,20 @@ Pagination.prototype = {
 		})
 
 	},
-	installListeners: function () { // вешаем событие клика на корневой элемент; устанавливаем обработчик; функция nofity; меняем модель и вызываем функцию render;
-		this.root.click( this.handleClick.bind(this) ); // клик на контейнер и обработчик с закреплением контекста this
+	installListeners: function () { 
+		this.root.click( this.handleClick.bind(this) ); 
 	},
 	handleClick: function(event) { // 
 		var current = this.current();
-		var target = $(event.target); // записали событие клика в переменную  
-		if ( !target.is('a[href]')) return; // если кликнули не по a, у которой есть href, то нам это не интересно
-		event.preventDefault(); // дефолтные браузеровские штуки событий заблокировали
-		if ( target.parent().hasClass('previous') ) { // если кликнули по стрелке предыдущий, то активный класс вешаем на активный - 1
+		var target = $(event.target); 
+		if ( !target.is('a[href]')) return; 
+		event.preventDefault(); 
+		if ( target.parent().hasClass('previous') ) { 
 			if ( current > 0) --current;
-		} else if ( target.parent().hasClass('next') ) { // если по следующей, то активная будет + 1
+		} else if ( target.parent().hasClass('next') ) { 
 			if ( current < this.total - 1) ++current;
 		} else {
-			current = +target.text() - 1; // по всех других случаях, активный будет текст страницы, преобразованный в число -1 (т.к. нумерация с нуля)
+			current = +target.text() - 1; 
 		}		
 
         this.current(current);
@@ -109,7 +109,7 @@ Pagination.prototype = {
 		if (this.onPageChange) this.onPageChange(this.current());
 	}
 }
-$(document).ready(function () { // создаем новый экземпляр класса
+$(document).ready(function () { 
 	var page = new Pagination('.pagination', 11, 4);
 	page.onPageChange = function(page) {
 		console.log(page);
